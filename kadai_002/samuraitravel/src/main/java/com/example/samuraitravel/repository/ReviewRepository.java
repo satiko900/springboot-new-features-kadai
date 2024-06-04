f1e914662ab4af2661321898f667e6dce1db8d64
satiko900 <r.ueno.0706@gmail.com>
2024-06-04 22:56:08 +0900

    kadai_002を追加

---
+++ kadai_002/samuraitravel/src/main/java/com/example/samuraitravel/repository/ReviewRepository.java
@@ -0,0 +1,15 @@
+package com.example.samuraitravel.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.samuraitravel.entity.Review;
+
+
+public interface ReviewRepository extends JpaRepository<Review, Integer>{
+	Page<Review> findByHouseId(Integer house, Pageable pageable);
+	List<Review> findByHouseId(Integer house);
+}
